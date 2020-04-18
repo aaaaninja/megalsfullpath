a339c442878e6e5f26e7b8116af4cfbc7d07b0fd
aaaaninja <aaaaninja@aaaa.ninja>
2020-04-18 19:33:19 +0900

    そして2つずつ配列indexを取り出してtargetフォルダ、ファイルのrangeを作り出す

--- fullpath.rb
+++ fullpath.rb
@@ -3,3 +3,5 @@ mega_ls_results = `mega-ls /ebooks/`.split "\n"
 p mega_ls_results.map
                  .with_index { |v, i| v.include?("\t") ? nil : i }
                  .compact
+                 .each_cons(2)
+                 .map { |s_idx, e_idx| mega_ls_results[s_idx...e_idx] }
